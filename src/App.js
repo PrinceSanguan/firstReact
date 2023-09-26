445201f79b21ec16f7de995ce7704496216deaaa
Prince Sanguan <princesanguan44@gmail.com>
2023-09-26 18:58:32 +0800

    Finish my First simple React Web

--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
+import searchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
 // 97b837be
 
-const API_URL = "http://www.omdapi.com?apikey=97b837be";
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=97b837be";
+
+const movie1 = {
+  Title: "Spiderman and Grandma",
+  Year: "2009",
+  imdbID: "tt1433184",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BMjE3Mzg0MjAxMl5BMl5BanBnXkFtZTcwNjIyODg5Mg@@._V1_SX300.jpg",
+};
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
   const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}$s=${title}`);
+    const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data);
+    setMovies(data.Search);
   };
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
 
-  return <h1>HEllo</h1>;
+  return (
+    <div className="app">
+      <h1>PrinceNetflix</h1>
+      <div className="search">
+        <input
+          placeholder="Search for Movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={searchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>Wala yung Movies Na Hinahanap mo!</h2>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
-
-// 50:30 Natapos!!!
