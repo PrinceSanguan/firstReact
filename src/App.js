088e47e046282c38296ea458deb1e0f6d879a5c1
Prince Sanguan <princesanguan44@gmail.com>
2023-09-29 20:59:04 +0800

    naayos na ang search bar nagdagdag na din ako ng movie

--- src/App.js
+++ src/App.js
@@ -14,22 +14,38 @@ const staticData = {
       SourceLink: "https://yugenanime.tv/watch/798/one-piece/1/",
     },
     {
-      Title: "Batman v Superman: Dawn of Justice",
-      Year: "2016",
-      Type: "movie",
+      Title: "Yowamushi Pedal Episode 1",
+      Year: "20019",
+      Type: "anime",
       Poster:
-        "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+        "https://animetourism88.com/application/files/3516/7827/0929/Web_YP_KV3.jpg",
       SourceLink:
-        "https://www.bilibili.tv/en/video/2005812974?bstar_from=bstar-web.homepage.recommend.all",
+        "https://www1.animixplayer.top/watch/yowamushi-pedal-episode-1-e9334",
     },
     {
-      Title: "The Batman",
-      Year: "2022",
+      Title: "Naruto Episode 1",
+      Year: "2008",
+      Type: "anime",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BZmQ5NGFiNWEtMmMyMC00MDdiLTg4YjktOGY5Yzc2MDUxMTE1XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_FMjpg_UX1000_.jpg",
+      SourceLink: "https://www7.gogoanime.me/naruto-episode-1",
+    },
+    {
+      Title: "Dragon Ball Z Episode 1",
+      Year: "1997",
+      Type: "anime",
+      Poster:
+        "https://resizing.flixster.com/zZRHfRmW-YrKjgXyV6sWYRlN3YU=/206x305/v2/https://flxt.tmsimg.com/assets/p186420_b_v8_aq.jpg",
+      SourceLink: "https://www.facebook.com/watch/?v=281939798884822",
+    },
+    {
+      Title: "3 Idiots",
+      Year: "2005",
       Type: "movie",
       Poster:
-        "https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_SX300.jpg",
+        "https://upload.wikimedia.org/wikipedia/en/d/df/3_idiots_poster.jpg",
       SourceLink:
-        "https://www.bilibili.tv/en/video/2040508241?bstar_from=bstar-web.homepage.recommend.all",
+        "https://www.bilibili.tv/en/video/2006735646?bstar_from=bstar-web.homepage.recommend.all",
     },
     // Add more movie objects as needed
   ],
@@ -39,9 +55,12 @@ const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
-  const searchMovies = async (title) => {
-    // Replace the API call with the static data
-    setMovies(staticData.Search);
+  const searchMovies = (title) => {
+    // Filter movies based on the search term
+    const filteredMovies = staticData.Search.filter((movie) =>
+      movie.Title.toLowerCase().includes(title.toLowerCase())
+    );
+    setMovies(filteredMovies);
   };
 
   const handleKeyPress = (e) => {
@@ -51,7 +70,8 @@ const App = () => {
   };
 
   useEffect(() => {
-    searchMovies("Spiderman");
+    // Initialize movies with all static data
+    setMovies(staticData.Search);
   }, []);
 
   return (
